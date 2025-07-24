c4ea04ab67534e1344fa6dbf872d67cb442c4a99
Amin Lakhani <alakhani@macrosoftinc.com>
2025-07-25 02:11:12 +0500

    main

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
   ],
     credentials: true
 }));
-
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
