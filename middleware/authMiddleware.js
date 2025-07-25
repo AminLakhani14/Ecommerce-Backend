238221a67021c9999ed3f46872b12c88b5188650
Amin Lakhani <alakhani@macrosoftinc.com>
2025-07-25 16:59:28 +0500

    main

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,33 +1,28 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/userModel.js';
 
-// Protect routes - checks for valid JWT
 const protect = async (req, res, next) => {
-  let token;
-
-  // Read JWT from the 'jwt' cookie
-  token = req.cookies.jwt;
-
+  let token = req.cookies.jwt;
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.userId).select('-password');
-      next(); // Proceed to the next middleware or controller
+      next();
     } catch (error) {
-      console.error(error);
-      res.status(401).json({ message: 'Not authorized, token failed' });
+      return res.status(401).json({ message: 'Not authorized, token failed' });
     }
   } else {
-    res.status(401).json({ message: 'Not authorized, no token' });
+    return res.status(401).json({ message: 'Not authorized, no token' });
   }
 };
 
-// Admin middleware - checks if user is an admin
 const admin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
+  const providedKey = req.headers.authorization;
+  if (providedKey && providedKey === process.env.ADMIN_SECRET_KEY) {
+    req.user = { _id: 'ADMIN_USER_ID_PLACEHOLDER', isAdmin: true };
     next();
   } else {
-    res.status(401).json({ message: 'Not authorized as an admin' });
+    res.status(401).json({ message: 'Not authorized as an admin.' });
   }
 };
 
